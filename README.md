5a8682e8480e897d10ca7a95583fa10125906232
suryaadev <infiniterohitsurya.com>
2022-09-10 14:27:29 +0530

    hardhat etherjs completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# ETHJS-SIMPLE-STORAGE
+
+contract address on Goerli : 0xCdA5dfd8b8Ae20AcEb27d998A0fb7bfe0f2Ca985
--- delpoy.js
+++ delpoy.js
@@ -13,10 +13,18 @@ async function main() {
    *
    */
 
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://192.168.0.107:7545"
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  //not doing this now adding encrypted key now
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+  // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
+
+  // let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PRIVATE_KEY_PASSWORD
+  // );
+  // wallet = await wallet.connect(provider);
+
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
   const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8");
 
@@ -24,6 +32,7 @@ async function main() {
   console.log("deploying, please wait....");
   const contract = await contractFactory.deploy();
   await contract.deployTransaction.wait(1);
+  console.log(`contract address: ${contract.address}`);
 
   /* waiting code and code for getting recipet
 
